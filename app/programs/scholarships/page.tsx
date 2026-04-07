2777d5d59befab4e66e67d8a55f3748656e0cb67
Adam Metcalf <adam@tenaciousdev.com>
2026-04-07 17:00:21 -0400

    fixed to include public directory at top of filepath AGAIN

--- app/programs/scholarships/page.tsx
+++ app/programs/scholarships/page.tsx
@@ -53,7 +53,7 @@ export default function ScholarshipPage() {
               <div className="flex w-full flex-col gap-3">
                 <Button asChild size="lg" className="w-full">
                   <a
-                    href="/public/Post-118-Scholarship-Guidelines.pdf"
+                    href="app\programs\scholarships\public\Post-118-Scholarship-Guidelines.pdf"
                     download
                   >
                     <Download className="mr-2 h-4 w-4" />
@@ -62,7 +62,7 @@ export default function ScholarshipPage() {
                 </Button>
                 <Button asChild size="lg" variant="outline" className="w-full">
                   <a
-                    href="/public/Post-118-Scholarship-Application.pdf"
+                    href="app\programs\scholarships\public\Post-118-Scholarship-Application.pdf"
                     download
                   >
                     <Download className="mr-2 h-4 w-4" />
